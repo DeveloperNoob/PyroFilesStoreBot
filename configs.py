62b20563c59e0bc5ed566d8b9cc43ce70d727f4d
DeveloperNoob <91716451+DeveloperNoob@users.noreply.github.com>
2021-11-21 16:27:34 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -30,18 +30,20 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 📡 **Hosted on:** [Heroku](https://heroku.com)
 
-🧑🏻‍ **Owner:** @Jackbro007
+🧑🏻‍💻 **Developer:** @AbirHasan2005
 
-🎬 **Support Group:** [Linux Repositories](https://t.me/DevsZone)
+👥 **Support Group:** [Linux Repositories](https://t.me/DevsZone)
 
-📺 **Series Group:** [TvSeriesLand4U](https://t.me/Discovery_Updates)
+📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
 """
 	ABOUT_DEV_TEXT = f"""
 🧑🏻‍💻 **Developer:** @AbirHasan2005
 
-remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
+Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 
+Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
+[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
